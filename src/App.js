56d61c1d64b72ac6e1df22a3e911d978dab32677
zhangzhengyang <zhangzhengyang27@aliyun.com>
2023-06-05 15:09:46 +0800

    banner 组件

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import 'normalize.css'
-import './style.scss';
-import styles from './style.module.scss';
+import Home from './container/Home'
 
-function App() {
-  return (
-    <div className={styles.app}>hello, zhangzhengyang!</div>
-  );
-}
+// function App() {
+//   return (
+//     <div className={styles.app}>hello, zhangzhengyang!</div>
+//   );
+// }
+
+const App = () => (<Home />)
 
 export default App;
